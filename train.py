92c1eb11678bd7e06c655747562f09726015c004
changenameYanjingzhu <yanjingzhu123@outlook.com>
2020-05-27 13:45:08 +0800

    Update train.py

--- train.py
+++ train.py
@@ -5,22 +5,3 @@ import model
 
 tournaments = nx.tournament_names()
 print(tournaments)
-try: 
-  # download dataset from numerai
-  data = nx.download('numerai_dataset.zip')
-except OSError as err:
-    print("OS error: {0}".format(err))
-except ValueError:
-    print("Could not convert data to an integer.")
-except:
-    print("Unexpected error:", sys.exc_info()[0])
-    raise
-for tournament_name in tournaments:
-    # create your model
-    m = model.LinearModel(verbose=True)
-
-    print("fitting model for", tournament_name)
-    m.fit(data['train'], tournament_name)
-
-    print("saving model for", tournament_name)
-    m.save('model_trained_' + tournament_name)
